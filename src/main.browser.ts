c32381efc63e8356d0ecc2c3ef4d9165bae0c8f3
katspaugh <ivan@mycs.com>
2016-08-02 10:21:58 +0200

    add langs

--- src/main.browser.ts
+++ src/main.browser.ts
@@ -1,3 +1,4 @@
+import {enableProdMode} from '@angular/core';
 import {LocationStrategy, PathLocationStrategy, HashLocationStrategy} from '@angular/common';
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {HTTP_PROVIDERS} from '@angular/http';
@@ -6,11 +7,13 @@ import {HTTP_PROVIDERS} from '@angular/http';
 import {APP_ROUTER_PROVIDERS} from './app/app.routes';
 import {App} from './app/app';
 
-// enableProdMode()
+if (/fluentcards\.com/.test(window.location.host)) {
+    enableProdMode()
+}
 
 bootstrap(App, [
   HTTP_PROVIDERS,
   APP_ROUTER_PROVIDERS,
-  { provide: LocationStrategy, useClass: HashLocationStrategy }
+  { provide: LocationStrategy, useClass: PathLocationStrategy }
 ])
 .catch(err => console.error(err));
