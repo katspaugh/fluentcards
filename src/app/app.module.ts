d685b0eb044263c4f374582a3135679005afe571
katspaugh <ivan@mycs.com>
2017-02-11 22:40:48 +0100

    revamp the site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,35 +12,28 @@ import {DictionaryService} from './services/dictionary';
 import {DefinitionsService} from './services/definitions';
 import {ImageSearchService} from './services/image-search';
 import {MassTranslationService} from './services/mass-translation';
-import {ContentfulService} from './services/contentful';
 
 import {StartView} from './components/start-view/start-view';
-import {IntroView} from './components/intro-view/intro-view';
-import {ExportView} from './components/export-view/export-view';
 import {UploadView} from './components/upload-view/upload-view';
 import {BooksView} from './components/books-view/books-view';
 import {Book} from './components/book/book';
-import {StoriesView} from './components/stories-view/stories-view';
-import {StoryView} from './components/story-view/story-view';
 
 import {BookList} from './components/book-list/book-list';
 import {Drop} from './components/drop/drop';
 import {Footer} from './components/footer/footer';
 import {Header} from './components/header/header';
 import {Loader} from './components/loader/loader';
-import {Sticky} from './components/sticky/sticky';
 import {VocabImages} from './components/vocab-images/vocab-images';
 
 @NgModule({
   declarations: [
-    AppComponent, StartView, IntroView, ExportView, UploadView, BooksView, Book,
-    BookList, Drop, Footer, Header, Loader, VocabImages,
-    StoriesView, StoryView, Sticky
+    AppComponent, StartView, UploadView, BooksView, Book,
+    BookList, Drop, Footer, Header, Loader, VocabImages
   ],
   imports     : [ BrowserModule, FormsModule, HttpModule, RouterModule.forRoot(rootRouterConfig) ],
   providers   : [
     VocabService, DictionaryService, DefinitionsService, ImageSearchService,
-    SpeechService, MassTranslationService, ContentfulService,
+    SpeechService, MassTranslationService,
     { provide: 'Window',  useValue: window }
   ],
   bootstrap   : [ AppComponent ]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,35 +1,25 @@
 import {Routes} from '@angular/router';
 
 import {StartView} from './components/start-view/start-view';
-import {IntroView} from './components/intro-view/intro-view';
 import {ExportView} from './components/export-view/export-view';
 import {UploadView} from './components/upload-view/upload-view';
 import {BooksView} from './components/books-view/books-view';
 import {Book} from './components/book/book';
-import {StoriesView} from './components/stories-view/stories-view';
-import {StoryView} from './components/story-view/story-view';
 
 export const rootRouterConfig: Routes = [
-  {
-    path: '', component: StartView,
+  { path: '', pathMatch: 'full', redirectTo: 'books' },
+  { path: 'demo', redirectTo: 'books' },
+
+  { path: 'kindle', component: StartView,
     children: [
-      { path: '', component: IntroView },
-      { path: 'kindle', component: ExportView },
-      { path: 'upload', component: UploadView }
+      { path: '', component: UploadView }
     ]
   },
-  { path: 'demo', redirectTo: 'books' },
+
   { path: 'books',
     children: [
       { path: '', component: BooksView },
       { path: ':id', component: Book }
     ]
-  },
-  { path: 'stories',
-    children: [
-      { path: '', component: StoriesView },
-      { path: ':id', component: StoryView }
-    ]
   }
-
 ];
