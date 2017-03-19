79e918438f2653c1740088f9224fb869ad64c2d1
katspaugh <ivan@mycs.com>
2017-03-19 13:27:27 +0100

    stripes for missing covers

--- src/app/components/book-list/book-list.ts
+++ src/app/components/book-list/book-list.ts
@@ -12,22 +12,37 @@ export class BookList {
   books: any[];
 
   private randomGradient() {
-    return this.sanitizer.bypassSecurityTrustStyle(
-      `linear-gradient(to bottom,
-hsl(${ Math.random() * 360 }, 80%, 80%) 0%,
-hsl(${ Math.random() * 360 }, 80%, 80%) 100%)`
+    const hueA = ~~(Math.random() * 360);
+    const hueB = ~~(Math.random() * 360);
+    const angleA = ~~(Math.random() * 360);
+    const angleB = angleA - 45;
+    const size = ~~(Math.random() * 100);
+
+    return (
+      `repeating-linear-gradient(
+         ${ angleA }deg,
+         hsla(${ hueA }, 80%, 40%, 0.7),
+         hsla(${ hueA }, 80%, 40%, 0.7) ${ size }px,
+         hsla(${ hueB }, 80%, 80%, 0.7) ${ size }px,
+         hsla(${ hueB }, 80%, 80%, 0.7) ${ size * 2 }px
+       ),
+       repeating-linear-gradient(
+         ${ angleB }deg,
+         hsla(${ hueB }, 80%, 40%, 0.1),
+         hsla(${ hueB }, 80%, 40%, 0.1) 2px,
+         hsla(${ hueA }, 80%, 80%, 0.1) 2px,
+         hsla(${ hueA }, 80%, 80%, 0.1) 4px
+       )`
     );
   }
 
   constructor(private sanitizer: DomSanitizer, private vocabService: VocabService) {
     this.books = this.vocabService.getBooks();
+  }
 
-    // Add gradients for missing covers
-    this.books.forEach((book) => {
-      if (!book.cover) {
-        book.gradient = this.randomGradient();
-      }
-    });
+  getGradient(book) {
+    book.gradient = book.gradient || this.randomGradient();
+    return this.sanitizer.bypassSecurityTrustStyle(book.gradient);
   }
 
   truncateWords(text, wordsCount) {
